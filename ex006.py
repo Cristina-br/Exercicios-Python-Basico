74e51e2b3e8adbd83499cbb2b0d5c85f5c5a2545
Cristina-br <151760169+Cristina-br@users.noreply.github.com>
2024-09-02 23:29:51 -0300

    Add files via upload

---
+++ ex006.py
@@ -0,0 +1,8 @@
+n = int(input("Digite um número: "))
+
+dobro_n = n * 2
+triplo_n = n * 3
+raiz_quad_n = pow(n, 1/2)
+
+print(f"O dobro de {n} vale {dobro_n}")
+print(f"O triplo de {n} vale {triplo_n} \nA raiz quadrada de {n} é igual a {raiz_quad_n:.2f}")
